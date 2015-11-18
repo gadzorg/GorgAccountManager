df0e7bae4dc7d21078c106eeb4c177d6ba137e70
Dorian BECKER <dorian.becker@gadz.org>
2015-11-18 12:26:05 +0100

    stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,5 +9,7 @@ class AdminController < ApplicationController
     @links_count = Uniqlink.all.count
     @sessions_count = Recoverysession.all.count
     @sms_count = Uniqsms.all.count
+
+    @top_hruid = Recoverysession.all.map{|r| r.hruid}.each_with_object(Hash.new(0)) { |hruid,counts| counts[hruid] += 1 }.sort_by{|hruid,count| count}.reverse.take(10)
   end
 end
