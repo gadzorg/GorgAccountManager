37279b3313aac7c49790096d2e0f657570dd9e1c
Dorian Becker <dorian.becker@gadz.org>
2016-01-29 12:10:10 +0100

    fix uniqlink creation

--- app/models/uniqlink.rb
+++ app/models/uniqlink.rb
@@ -1,8 +1,9 @@
 class Uniqlink < ActiveRecord::Base
 	before_save :default_values
-	  def default_values
-	    self.inscription ||= false
-	  end
+	def default_values
+		self.inscription ||= false
+		true
+	end
 
 	def generate_token
 		self.token = loop do
