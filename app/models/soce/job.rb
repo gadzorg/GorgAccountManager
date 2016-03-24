ea6c7511bb64dd9d75ffdc62b0ec04255662559c
Dorian Becker <dorian.becker@gadz.org>
2016-03-24 17:40:36 +0100

    job model fix

--- app/models/soce/job.rb
+++ app/models/soce/job.rb
@@ -7,7 +7,7 @@ class Soce::Job < Soce::Base
 	has_one :list_reseaux_sociaux, foreign_key: "id_reseau_social"
 
 	def self.serialize
-		hruid = self.first.usersoce.hruid
+		hruid = self.first.user.hruid
 		sql = "SELECT *,e.*, p.date_debut, p.date_fin, p.tel_direct, p.tel_standard, p.email, p.gsm, p.adresse, p.adresse2, p.adresse3, p.code_postal AS code_postal_entreprise, p.ville AS ville_entreprise, p.pays AS pays_entreprise, p.fax, p.id_etat_validation, py.*, f.* FROM users 
         left join postes AS p on users.id_user = p.id_user
         left join entreprises AS e on e.id_entreprise = p.id_entreprise
