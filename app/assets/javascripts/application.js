2b795982e238359dd9399718cbc57ce3d7e7aa99
blaked <dorian.becker@gadz.org>
2017-03-29 11:12:05 +0200

    :lipstick: ui fix + recovery support update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,5 +45,10 @@ $(document).ready(function () {
              };
            }
     );
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 });
 
