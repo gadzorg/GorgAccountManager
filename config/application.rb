fb3a61e21338d5b71c673a91a38994855c8871d9
blaked <dorian.becker@gadz.org>
2017-04-26 20:30:22 +0200

    :wrench: clean code

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'active_resource'
-#require "gorg_engine"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
