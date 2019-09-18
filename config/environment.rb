4a87abc4d5e37f9e09c7bb601359d35d92b3e324
Colin Darie <colin@darie.eu>
2019-09-18 12:38:19 +0200

    Move smtp settings in an initializer

--- config/environment.rb
+++ config/environment.rb
@@ -7,13 +7,3 @@ ActiveResource::Base.logger = ActiveRecord::Base.logger
 
 ENV['INLINEDIR'] = File.join(File.dirname(__FILE__),'../public/
 uploads/')
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => Rails.application.secrets.smtp_user,
-  :password => Rails.application.secrets.smtp_password,
-  :domain => 'localhost',
-  :address => Rails.application.secrets.smtp_adress,
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
